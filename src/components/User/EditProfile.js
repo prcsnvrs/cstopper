86a79e121348f91f3b1826aaa95999971dc8ab9e
prcsnvrs <154627240+prcsnvrs@users.noreply.github.com>
2024-01-03 01:34:50 +0800

    Update EditProfile.js

--- src/components/User/EditProfile.js
+++ src/components/User/EditProfile.js
@@ -2,9 +2,7 @@ import React from "react";
 import "./design/dashboard.css";
 import Sidebar from "./Sidebar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUserCircle,
-} from "@fortawesome/free-solid-svg-icons";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 
 const Dashboard = () => {
   return (
@@ -12,58 +10,60 @@ const Dashboard = () => {
       <Sidebar />
       <div className="content">
         <div className="texts">
-            <h1>Profile</h1>
-            <div className="form-container">
-          <div className="user-container">
-            <div className="user-icon">
-              <FontAwesomeIcon
-                icon={faUserCircle}
-                style={{ color: "white", fontSize: "90px" }}
-              />
+          <h1>Profile</h1>
+          <div className="form-container">
+            <div className="user-container">
+              <div className="user-icon">
+                <FontAwesomeIcon
+                  icon={faUserCircle}
+                  style={{ color: "white", fontSize: "90px", textAlign: "center", paddingTop: "20px" }}
+                />
+              </div>
+              <div className="user-info">
+                <span style={{ color: "white", fontSize: "23px" }}>
+                  Username
+                </span>
+              </div>
             </div>
-            <div className="user-info">
-              <span style={{ color: "white", fontSize: "20px" }}>Username</span>
+            <form className="profile-form ">
+              <div className="form-group">
+                <label htmlFor="name">Name:</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="phoneNumber">Phone Number:</label>
+                <input
+                  type="text"
+                  id="phoneNumber"
+                  name="phoneNumber"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Change Password:</label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                />
+              </div>
+              <div
+              style={{
+                display: "flex",
+                justifyContent: "center"
+              }}
+            >
+              <button type="submit" className="button">
+                SAVE CHANGES
+              </button>
             </div>
+            </form>
           </div>
-          <form className="profile-form ">
-            <div className="form-group">
-              <label htmlFor="name">Name:</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                placeholder="Enter your name"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="phoneNumber">Phone Number:</label>
-              <input
-                type="text"
-                id="phoneNumber"
-                name="phoneNumber"
-                placeholder="Enter your phone number"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Change Password:</label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Enter new password"
-              />
-            </div>
-            <button type="submit" className="centered-button submit-button">
-              <span
-                style={{ color: "white", fontSize: "20px", paddingTop: "5px" }}
-              >
-                Save Changes
-              </span>
-            </button>
-          </form>
         </div>
-      </div>        
-    </div>
+      </div>
     </div>
   );
 };
