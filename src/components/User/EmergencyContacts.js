99307ce442caff5f6286e0149512231fbd845daf
prcsnvrs <154627240+prcsnvrs@users.noreply.github.com>
2024-01-03 00:29:45 +0800

    Update EmergencyContacts.js

--- src/components/User/EmergencyContacts.js
+++ src/components/User/EmergencyContacts.js
@@ -12,15 +12,15 @@ const Dashboard = () => {
         <div className="texts">
           <h1>Emergency Contacts</h1>
           <div className="subtxt">List of Emergency Police Contacts</div>
-          <div className="outer-container"/>
+          <div className="outer-container">
             <div className="subcon">
               <div className="user-icon">
                 <FontAwesomeIcon
                   icon={faPhone}
                   style={{ color: "white", fontSize: "30px" }}
                 />
-                <div className="contactname">Carbon, Cebu City</div>
-                <div className="contactnum">09236446496</div>
+                <div className="contactname">Mabolo Police Station</div>
+                <div className="contactnum">09325924464</div>
               </div>
             </div>
             <div className="subcon">
@@ -33,6 +33,7 @@ const Dashboard = () => {
                 <div className="contactnum">09164216215</div>
               </div>
             </div>
+          </div>
         </div>
       </div>
     </div>
