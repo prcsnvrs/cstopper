0a35df3b7dfbe3fae70116694d008cc48b3d639c
prcsnvrs <154627240+prcsnvrs@users.noreply.github.com>
2024-01-02 19:14:04 +0800

    Update EmergencyContacts.js

--- src/components/User/EmergencyContacts.js
+++ src/components/User/EmergencyContacts.js
@@ -1,13 +1,38 @@
 import React from "react";
-import './design/dashboard.css';
-import Sidebar from "./Sidebar"
+import "./design/dashboard.css";
+import Sidebar from "./Sidebar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+
 const Dashboard = () => {
   return (
-    <div className='dash'>
+    <div className="dashboard">
       <Sidebar />
-      <div className='body'>
-        <div className='texts'>
+      <div className="content">
+        <div className="texts">
           <h1>Emergency Contacts</h1>
+          <div className="subtxt">List of Emergency Police Contacts</div>
+          <div className="outer-container"/>
+            <div className="subcon">
+              <div className="user-icon">
+                <FontAwesomeIcon
+                  icon={faPhone}
+                  style={{ color: "white", fontSize: "30px" }}
+                />
+                <div className="contactname">Carbon, Cebu City</div>
+                <div className="contactnum">09236446496</div>
+              </div>
+            </div>
+            <div className="subcon">
+              <div className="user-icon">
+                <FontAwesomeIcon
+                  icon={faPhone}
+                  style={{ color: "white", fontSize: "30px" }}
+                />
+                <div className="contactname">Parian Police Station</div>
+                <div className="contactnum">09164216215</div>
+              </div>
+            </div>
         </div>
       </div>
     </div>
